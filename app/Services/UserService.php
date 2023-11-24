ce43c413359432bed83575b6fbca73256013679f
Jorge Carlos <skay_1994@yahoo.com.br>
2023-11-24 19:37:00 -0300

    Chore: Create method login in UserService

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,7 +2,29 @@
 
 namespace App\Services;
 
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Hash;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
 class UserService
 {
-    //
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'token' => $user->createToken('API Token')->plainTextToken,
+        ]);
+    }
 }
