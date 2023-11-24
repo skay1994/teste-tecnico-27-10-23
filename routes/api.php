1a91793d18a1878c19bbf19a9dc3d0542a356ab6
Jorge Carlos <skay_1994@yahoo.com.br>
2023-11-24 19:48:00 -0300

    Chore: Create login route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::name('auth.')->prefix('auth')->group(function () {
+    Route::post('login', LoginController::class)->name('login');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
