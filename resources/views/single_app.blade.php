041e06ed78d0ecff47a68a93e45132867368fe88
Jorge Carlos <skay_1994@yahoo.com.br>
2023-11-25 19:58:24 -0300

    Fix: Changing app.js to app.ts

--- resources/views/single_app.blade.php
+++ resources/views/single_app.blade.php
@@ -6,7 +6,7 @@
 
     <title>Laravel</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.ts'])
 </head>
 <body id="app">
 </body>
