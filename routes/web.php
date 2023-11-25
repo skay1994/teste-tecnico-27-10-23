590d96b8b67375c08bb3fbe41e85ffa07d21f232
Jorge Carlos <skay_1994@yahoo.com.br>
2023-11-25 19:57:21 -0300

    Chore: Add route for Vue app

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', static function () {
+    return view('single_app');
+});
+Route::get('/{any}', static function () {
+    return view('single_app');
 });
