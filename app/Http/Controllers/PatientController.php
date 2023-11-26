f84943b8ddf3607478f66cb4adc177ec2baba1a2
Jorge Carlos <skay_1994@yahoo.com.br>
2023-11-26 17:38:25 -0300

    Chore:  Create controller for Patients

---
+++ app/Http/Controllers/PatientController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePatientRequest;
+use App\Http\Requests\UpdatePatientRequest;
+use App\Models\Patient;
+use App\Services\PatientService;
+use Illuminate\Http\JsonResponse;
+
+class PatientController extends Controller
+{
+    public function __construct(
+        readonly protected PatientService $service
+    )
+    {
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    public function store(StorePatientRequest $request): JsonResponse
+    {
+        //
+    }
+
+    public function update(UpdatePatientRequest $request, Patient $patient)
+    {
+        //
+    }
+
+    public function destroy(Patient $patient)
+    {
+        //
+    }
+}
