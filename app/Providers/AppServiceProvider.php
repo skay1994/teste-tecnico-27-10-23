5a4034026693b7070a336ac0d00018b7cee01d43
Jorge Carlos <skay_1994@yahoo.com.br>
2023-11-26 19:53:00 -0300

    Chore: Add bind to Elasticsearch client

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(Client::class, function () {
+            $configs = config('elasticsearch');
+            return ClientBuilder::create()->setHosts([$configs['host']])->build();
+        });
     }
 
     /**
