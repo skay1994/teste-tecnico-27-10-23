eb90f872f824860860af154f78c45a25256c188f
Jorge Carlos <skay_1994@yahoo.com.br>
2023-11-26 19:23:37 -0300

    Chore:  Add method update for Patients

--- app/Services/PatientService.php
+++ app/Services/PatientService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Http\Requests\StorePatientRequest;
+use App\Http\Requests\UpdatePatientRequest;
 use App\Http\Resources\PatientResource;
 use App\Models\Patient;
 use Illuminate\Http\JsonResponse;
@@ -13,7 +14,11 @@ class PatientService
     {
         $patient = Patient::create($request->safe()->except('address', 'photo'));
 
-        $request->file('photo')->store();
+        $photo = $request->file('photo')->store();
+
+        $patient->update([
+            'photo' => $photo
+        ]);
 
         if ($request->has('address')) {
             $addresses = $request->get('address');
@@ -21,5 +26,27 @@ class PatientService
         }
 
         return response()->json(new PatientResource($patient), 201);
+    }
+
+    public function update(Patient $patient, UpdatePatientRequest $request): JsonResponse
+    {
+        $patient->fill($request->safe()->except('address', 'photo'));
+
+        if(\Storage::fileExists($patient->photo)) {
+            \Storage::delete($patient->photo);
+        }
+
+        $photo = $request->file('photo')->store();
+
+        $patient->photo = $photo;
+        $patient->save();
+
+        if ($request->has('address')) {
+            $patient->addresses()->delete();
+            $addresses = $request->get('address');
+            $patient->addresses()->createMany($addresses);
+        }
+
+        return response()->json(new PatientResource($patient));
     }
 }
