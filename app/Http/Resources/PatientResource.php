5998af17bc3e9d73c29d9b69c4efa993b5088902
Jorge Carlos <skay_1994@yahoo.com.br>
2023-11-26 20:08:00 -0300

    Chore: Update PatientResource

--- app/Http/Resources/PatientResource.php
+++ app/Http/Resources/PatientResource.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources;
 use App\Models\Patient;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
+use Storage;
 
 /** @mixin Patient */
 class PatientResource extends JsonResource
@@ -12,17 +14,15 @@ class PatientResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
+            'id' => $this->getKey(),
             'document' => $this->document,
             'cns' => $this->cns,
-            'photo' => $this->photo,
+            'photo' => Str::startsWith($this->photo, 'http') ? $this->photo : Storage::url($this->photo),
             'name' => $this->name,
             'mother_name' => $this->mother_name,
             'birthdate' => $this->birthdate,
             'phone' => $this->phone,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'addresses' => PatientAddressResource::collection($this->addresses),
+            'addresses' => PatientAddressResource::collection($this->whenLoaded('addresses')),
         ];
     }
 }
