2c075e2574352b22437044eb8f019ae665466ad4
Jorge Carlos <skay_1994@yahoo.com.br>
2023-11-27 11:17:00 -0300

    Chore: Create Spinner component

---
+++ resources/js/components/Spinner.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-white">
+        <div class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+    </div>
+</template>
